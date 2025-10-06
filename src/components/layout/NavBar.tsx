f91cac5d919d963a31a1c04834e130f2c087304c
ZippyBonhTM <zippybont@gmail.com>
2025-10-06 18:06:33 -0300

    NavBar new visual

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -9,18 +9,24 @@ export default function NavBar() {
   const homeHandler = () => {
     redirect("/");
   };
+  const periodicHandler = () => {
+    redirect("/periodic");
+  }
 
   return (
     <div className="flex flex-col mb-2 mx-auto w-[50%] px-2 pb-2 pt-0.5 gap-0.5 bg-neutral-900 border-x border-b rounded-b-xl border-neutral-600 z-500">
       <p className="flex justify-center items-center truncate whitespace-nowrap h-4 overflow-hidden uppercase text-sm">{PageName}</p>
       <div className="flex justify-between w-full">
-        <section>
+        <section className="flex justify-stat w-[33%]">
           <button onClick={homeHandler} className="bg-neutral-200 border p-1.5 rounded-2xl text-black">Home</button>
+          <button onClick={periodicHandler} className="bg-neutral-200 border p-1.5 rounded-2xl text-black">Periodic Table</button>
         </section>
-        <section>
-          <p className="bg-neutral-700 border p-1.5 rounded-2xl">Mei</p>
+        <section className="flex justify-center w-[33%]">
+          <p className="bg-neutral-700 border p-1.5 rounded-2xl">Meio</p>
         </section>
-        <section><User /></section>
+        <section className="flex justify-end w-[33%]">
+          <User />
+        </section>
       </div>
     </div>
   );
